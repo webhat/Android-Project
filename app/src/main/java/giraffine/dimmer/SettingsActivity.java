494cf17cac558746f79ebb05dce82cf19f05f1f1
Daniël W. Crompton <daniel.crompton@gmail.com>
2020-02-03 11:46:35 +0100

    Fixed so it runs other places than my machine ;D - changed formatting - changed notification text colour - version bump

--- app/src/main/java/giraffine/dimmer/SettingsActivity.java
+++ app/src/main/java/giraffine/dimmer/SettingsActivity.java
@@ -3,27 +3,26 @@ package giraffine.dimmer;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SettingsActivity extends Activity{
+public class SettingsActivity extends Activity {
 
-	public static boolean showSettings = false;
+    public static boolean showSettings = false;
 
-	@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(getActionBar() != null)
-        	getActionBar().hide();
+        if (getActionBar() != null)
+            getActionBar().hide();
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
         showSettings = true;
     }
-	
-	public void onPause ()
-	{
-		super.onPause();
-		showSettings = false;
-		finish();
-	}
+
+    public void onPause() {
+        super.onPause();
+        showSettings = false;
+        finish();
+    }
 }
